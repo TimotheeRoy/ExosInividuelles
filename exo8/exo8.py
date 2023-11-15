21dd969fd639abae63c2ea32b14c4d7db8047318
TimotheeRoy <timroy93100@gmail.com>
2023-11-15 14:15:10 +0100

    update exo8

--- exo8/exo8.py
+++ exo8/exo8.py
@@ -1,6 +1,7 @@
 ## on rentre un tableau de 4 couleurs. En sortie on a un tableau de 2 chiffres
 ## le premier chiffre représente le nb de bonne couleur bien placée, le 2e le nb de bonne couleur mal placée 
 from random import *
+from collections import Counter
 
 
 colors = ['red', 'green', 'blue', 'yellow','purple','grey','black','white']
@@ -25,30 +26,38 @@ def checkPropal (tab,code):
 ## condition de victoire, true si propal = code, false sinon
 
 def hint(tab,code):
-    colorAndPos , color = 0, 0          
+    colorAndPos , color = 0, 0 
+    setCode, setTab = set(code), set(tab)  
+
     for i in range(len(tab)):
         if tab[i] == code[i]:
             colorAndPos+=1
-        elif tab[i] in code:
-            color+=1                  
-    return [colorAndPos, color]
+        
+    counterCode, counterTab = Counter(code), Counter(tab)
+    for colorToFind in counterCode:
+        for colorToCheck in counterTab:
+            if colorToFind == colorToCheck:
+                color += min(counterCode[colorToFind], counterTab[colorToCheck])
+        #elif tab[i] in code:
+        #    color+=1                  
+    return ['Bonnes couleurs à la bonne place : ' + str(colorAndPos),
+            'Bonnes couleurs à la mauvaise place : '+ str(color-colorAndPos)]
 
-## code =[red,red]
-## propal = [red, green] -> hint() ->
+
 
 def askColor():
     return input('Entrez une couleur : ')
 
-def gameLoop(n):            #n est le nombre de couleurs à trouver 
-    code = defineCode()
-    print(code)
+def gameLoop():            
+    #code = defineCode()
+    code = ['black', 'black', 'purple', 'white']
     while True:
 
-        propal = [askColor(), askColor(), askColor(), askColor()]
+        propal = [askColor() for _ in range(4)]
         print('Proposition : ', propal)
         while not isColors(propal):
             print('Les couleurs possibles sont : ',colors)
-            propal = [askColor(), askColor(), askColor(), askColor()]
+            propal = [askColor() for _ in range(4)]
         
         if checkPropal(propal,code):
             return print('Tu as trouvé')
@@ -57,4 +66,4 @@ def gameLoop(n):            #n est le nombre de couleurs à trouver
         print(hint(propal,code))
     
     
-gameLoop(4)
+gameLoop()
