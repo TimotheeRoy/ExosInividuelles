3905f7f3e0830a31a775df8a5f99bd85d5643395
TimotheeRoy <timroy93100@gmail.com>
2023-11-30 15:25:55 +0100

    premiere implémentation html/css

--- exo9/exo9.js
+++ exo9/exo9.js
@@ -1,21 +1,74 @@
+const space = ' ';
+
 const quotient = (a, b) => {
-    return Math.floor(a / b)
+    return Math.floor(a / b);
 }
 
-const displayLineForChristmasTree = (line, n) => {
-    return ' '.repeat(n + 1) + '/' + '*'.repeat(line * 2 - 1) + '\\' + '\n';
+const displayBoulesDeNoelouPasBoulesDeNoel = (freq) => {
+    const tabDeco = ['*', 'o', '+'];
+    for (let i = 0; i < freq; i++)
+        tabDeco.push('*')
+    return tabDeco[Math.floor(Math.random() * tabDeco.length)];
 }
 
-const sapin = (n) =>{
-    let tree = ' '.repeat(n+1)+'+\n';
-    for (let i=1; i<=n+1 ; i++){
-        let offset = quotient(i-1,3)
-        tree += displayLineForChristmasTree(i-offset,n-i+offset)
+const displayLeafsForXmasTree = (n) => {
+    let leafsLine = '*'
+    for (let i = 1; i < n - 1; i++) {
+        leafsLine += displayBoulesDeNoelouPasBoulesDeNoel(3)
+    }
+    return leafsLine + '*'
+}
+
+const displayLineForXmasTree = (line, n) => {
+    if (line === 1) {
+        return space.repeat(n + 1) + '/' + '*' + '\\' + '\n';
+    }
+    else
+        return space.repeat(n + 1) + '/' + displayLeafsForXmasTree(line * 2 - 1) + '\\' + '\n';
+}
+
+const dislayTrunkForXmasTree = (n, offset) => {
+    const trunkSpaces = (n < 3) ? space.repeat(n + 1) : space.repeat(n);
+    const trunk = '#'.repeat((offset + 1 <= 2) ? offset + 1 : 2);
+    return trunkSpaces + trunk + '\n';
+}
+
+const sapin = (n) => {
+    let tree = space.repeat(n + 1) + '+\n';
+    let offset;
+
+    //affichage du feuillage 
+    for (let i = 1; i <= n + 1; i++) {
+        offset = quotient(i - 1, 3);
+        tree += displayLineForXmasTree(i - offset, n - i + offset);
+    }
+
+    //affichage du tronc
+    for (let i = 1; i <= offset + 1; i++) {
+        tree += dislayTrunkForXmasTree(n, offset);
+    }
+
+    return tree;
+}
+
+
+const btn = document.querySelector('#btn')
+const inputNumber = document.querySelector('input')
+const tree = document.querySelector('#tree')
+
+inputNumber.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter'){
+        tree.innerHTML = '';
+        const XmasTree = sapin(parseInt(inputNumber.value)-1).replace(/\n/g, '<br>')
+        tree.innerHTML += `<pre>${XmasTree} </pre>`
     }
     
-    return tree
-}
-
-console.log(sapin(7))
+})
 
 
+btn.addEventListener('click', (e) => {
+    tree.innerHTML = '';
+    const XmasTree = sapin(parseInt(inputNumber.value)-1).replace(/\n/g, '<br>')
+    tree.innerHTML += `<pre>${XmasTree} </pre>`
+    
+})
